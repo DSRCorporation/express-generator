6ded4be5c12c081d1bc6a2703975f9a62a757da9
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-02-06 17:24:28 +0300

    Added model for sequelize

--- generators/app/templates/app/libs/models/User.jwtSupport-addon.js
+++ generators/app/templates/app/libs/models/User.jwtSupport-addon.js
@@ -1,7 +1,11 @@
 'use strict';
 
-const mongoose = require('utils/mongoose'),
-    promise = require('utils/promise'),
+const promise = require('utils/promise'),
+    <% if (locals.useMongo) {%>
+    mongoose = require('utils/mongoose'),<%}%>
+    <% if (locals.useSequelize) {%>
+    sequelize = require('utils/sequelize'),
+    Sequelize = require('sequelize'),<%}%>
     bcrypt = require('utils/bcrypt'),
     SALT_WORK_FACTOR = 10;
 
@@ -9,40 +13,53 @@ const mongoose = require('utils/mongoose'),
  * User entity scheme.
  * @type {*|Schema}
  */
-let userSchema = new mongoose.Schema({
-    login: {
-        type: String,
-        required: true,
-        index: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-});
+let userSchema;
+
+<% if (locals.useMongo) {%>
+    userSchema = new mongoose.Schema({
+        login: {
+            type: String,
+            required: true,
+            index: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    });
+
+    /**
+     * Save-hook that fills password field with bcrypt hash.
+     */
+    userSchema.pre('save', promise.asyncHook(async function (user) {
+
+        if (!user.isModified('password')) {
+            return;
+        }
+
+        user.password = await bcrypt.hashAsync(user.password, await bcrypt.genSaltAsync(SALT_WORK_FACTOR));
+    }));
 
 
-/**
- * Save-hook that fills password field with bcrypt hash.
- */
-userSchema.pre('save', promise.asyncHook(async function (user) {
+    /**
+     * Verifies that password is equal to password of this user.
+     * @param candidatePassword password to verify.
+     * @return promised true if equals, false otherwise.
+     */
+    userSchema.methods.comparePasswordAsync = async function (candidatePassword) {
+        return await bcrypt.compareAsync(candidatePassword || '', this.password);
+    };
 
-    if (!user.isModified('password')) {
-        return;
-    }
+    module.exports = mongoose.model('User', userSchema);
+<%}%>
+<% if (locals.useSequelize) {%>
+    let userSchema = {
+            login: Sequelize.STRING,
+            password: Sequelize.STRING
+        };
+    module.exports = sequelize.define('User', userSchema);
+<%}%>
 
-    user.password = await bcrypt.hashAsync(user.password, await bcrypt.genSaltAsync(SALT_WORK_FACTOR));
-}));
 
 
-/**
- * Verifies that password is equal to password of this user.
- * @param candidatePassword password to verify.
- * @return promised true if equals, false otherwise.
- */
-userSchema.methods.comparePasswordAsync = async function (candidatePassword) {
-    return await bcrypt.compareAsync(candidatePassword || '', this.password);
-};
-
-module.exports = mongoose.model('User', userSchema);
