a65c57a236be1ce532e40f35d10470b599c2264d
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-02-15 14:55:24 +0300

    refactoring

--- generators/app/templates/app/libs/utils/redis.includeRedis-addon.js
+++ generators/app/templates/app/libs/utils/redis.includeRedis-addon.js
@@ -1,23 +1,38 @@
 'use strict';
 
-/**
- * Configured redis instance.
- */
+const promise = require('utils/promise'),
+    logger = require('utils/logger').utils,
+    redisConf = require('utils/config').get('redis'),
+    errors = require('errors'),
+    _ = require('lodash'),
+    redis = require('redis');
 
-var config = require('utils/config').get('redis'),
-    redis = require('redis'),
-    BPromise = require('bluebird');
+promise.promisifyAll(redis.RedisClient.prototype);
 
-module.exports.createClient = function () {
-    var client = redis.createClient(config.port, config.host, config.options);
-    BPromise.promisifyAll(client);
-    return client;
+async function createClient() {
+    return await new Promise(function(resolve) {
+        logger.info(`redis.createClient -> Connecting ${redisConf.host}:${redisConf.port}`);
+        let client = redis.createClient(_.merge(redisConf, {retry_strategy: retryStrategy}));
+
+        client.on('connect', function() {
+            logger.info(`redis.createClient -> Success ${redisConf.host}:${redisConf.port}`);
+            resolve(client);
+        });
+    });
+}
+
+function retryStrategy(options) {
+    let attempt = options.attempt,
+        maxAttempts  = redisConf.reconnect.maxAttempts;
+
+    logger.error(`redis.retryStrategy -> Trying to reconnect. Attempt ${attempt}`);
+    if (attempt < maxAttempts) {
+        return redisConf.reconnect.timeout;
+    }
+    return new errors.InternalServerError(`redis.retryStrategy -> Reconnecting failed after ${maxAttempts} attempts.`,
+        logger);
+}
+
+module.exports = {
+    createClient: createClient
 };
-
-module.exports.createMultiClient = function () {
-    var client = redis.createClient(config.port, config.host, config.options);
-    client = client.multi();
-    BPromise.promisifyAll(client);
-    return client;
-};
-
