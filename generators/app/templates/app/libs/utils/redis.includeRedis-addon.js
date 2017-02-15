ea380a6e59e926a3f3feac37e405f801fc3059fb
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-02-15 19:09:05 +0300

    refactoring

--- generators/app/templates/app/libs/utils/redis.includeRedis-addon.js
+++ generators/app/templates/app/libs/utils/redis.includeRedis-addon.js
@@ -6,11 +6,6 @@ const promise = require('utils/promise'),
     redisConfig = require('utils/config').get('redis'),
     redis = require('redis');
 
-const clients = {
-    publish: null,
-    subscribe: null
-};
-
 promise.promisifyAll(redis.RedisClient.prototype);
 promise.promisifyAll(redis.Multi.prototype);
 
@@ -18,32 +13,7 @@ function createClient() {
     return redis.createClient(redisConfig.port, redisConfig.host, redisConfig.options);
 }
 
-function createMultiClient() {
-    return redis.createClient(redisConfig.port, redisConfig.host, redisConfig.options).multi();
-}
-
-async function initClients() {
-    return await promise.Promise.all(_.map(clients, function(client, clientName) {
-        return new promise.Promise((resolve, reject) => {
-            let newClient = createClient();
-
-            newClient
-                .on('connect', () => {
-                    logger.info(`Redis ${clientName} connecting -> done`);
-                    clients[clientName] = newClient;
-                    resolve(newClient);
-                })
-                .on('error', (err) => {
-                    logger.error(`Redis ${clientName} connecting -> error`, err);
-                    reject(err);
-                })
-        });
-    }));
-}
-
 module.exports ={
-    createClient: createClient,
-    createMultiClient: createMultiClient,
-    initClients: initClients,
-    clients: clients
-}
+    client: createClient(),
+    pubSubClient: createClient()
+};
